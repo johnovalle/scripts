03126a81798bbaa8d9b5b45cb89fdaec89253dd1
John Ovalle <johnovalle@gmail.com>
2014-06-01 02:28:55 +0000

    cleaned up scraper

--- kanjiscrape.rb
+++ kanjiscrape.rb
@@ -4,7 +4,6 @@
 
 require 'nokogiri'
 require 'open-uri'
-#html = open("http://jisho.org/words?jap=#{URI::encode('氷河')}&eng=&dict=edict")
 kanjis = [] #file.open kanji file
 duplicates = []
 File.open('kanji.txt', 'r').each { |line| kanjis << line.chomp }
@@ -36,41 +35,4 @@ write_file.close
 duplicates.uniq!
 puts duplicates.length
 puts duplicates
-#p address
-#BASE_URL = "http://jisho.org/words?jap="
-#list = ['予', '期', '感化']
-#list2 = list.map { |trans| URI::encode(trans) }
-#list2.each do |word|
-  
-#  html = open("#{BASE_URL}#{word}&eng=&dict=edict")
-#  page = Nokogiri::HTML(html.read)
-#  page.delete('.lower')
-#  puts page.css('#result_content #word_result tr').text #if page.css('#word_result tr').text.match(/#{word}/)
-#  sleep(1.0 + rand)
-#end
-#TO DO remove .lower nodes and transform garbage into kanji... how?
-#page = Nokogiri::HTML(open("http://jisho.org/words?jap=感&eng=&dict=edict"))
-#puts URI::encode('感')
-#page = Nokogiri::HTML(open("http://en.wikipedia.org/wiki/List_of_Nobel_laureates"))
-#puts page
-#puts page.css('title')
-#puts page.css('li')
-#puts page.css('li')[0].text
-#puts page.css('li')[0].css('a')[0]['href']
-#puts page.css("li a[data-category='news']")
-#puts page.css("li").text
-#puts page.css('div#funstuff')
-#page.css('div#references a').each do |link|
-#  puts link['href']
-#end 
-#links = []
-#page.css("table span.fn a").each do |link|
-#  links << link['href'] if link['href'].match(/wiki/)
-#end
-#puts links.length
-#links.uniq
-#puts links.length
-
-#puts links
-
 
